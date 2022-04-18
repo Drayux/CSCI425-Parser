c0b9cdc83270bd4469bd21d778ec038db21510f1
konch11037 <koncharjm@gmail.com>
2022-04-17 22:12:20 -0600

    fix the backslack

--- NFA.py
+++ NFA.py
@@ -136,7 +136,9 @@ class TTable:
 			print(f"WARNING: fromState: {fromState}, toState: {toState}")
 			raise ValueError("State IDs must be non-negative")
 		if type(transitionChar) is str:
-			if len(transitionChar) == 2:
+			if transitionChar == r'\\':
+				transitionChar = ord('\\')
+			elif len(transitionChar) == 2:
 				transitionChar = ord(transitionChar[1])
 			else:
 				transitionChar = ord(transitionChar[0])
