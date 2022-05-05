1847535811891d6f32ff3bebd8731b06db4bf64c
John Konchar <koncharjm@gmail.com>
2022-05-05 11:51:07 -0600

    Changed Paramlist killing params6

--- SymbolTable.py
+++ SymbolTable.py
@@ -118,7 +118,7 @@ class SymbolTable():
             f_id = remove_prefix(fnsig_node.children[1], "id:")
             # Get fn parameters from PARAMLIST node
             pl_node = verify_node(fnsig_node.children[2], "PARAMLIST")
-            if len(pl_node.children) == 1 : return
+            if pl_node.children[0].data == "NOPARAMS" : return
             for child in pl_node.children:
                 # Get individual fn parameter from a PARAM node
                 param_node = verify_node(child, "PARAM")
@@ -170,6 +170,7 @@ class SymbolTable():
             f_id = remove_prefix(fnsig_node.children[1], "id:")
             # Get fn parameters from PARAMLIST node
             pl_node = verify_node(fnsig_node.children[2], "PARAMLIST")
+            if pl_node.children[0].data == "NOPARAMS" : return
             for child in pl_node.children:
                 # Get individual fn parameter from a PARAM node
                 param_node = verify_node(child, "PARAM")
