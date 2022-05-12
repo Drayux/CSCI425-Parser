8a7d855743fda93879201b6b27316d2709e74c64
Chris Muller <chris.muller@outlook.com>
2022-05-12 11:03:39 -0600

    Data Segment Tweaks

--- DataSegment.py
+++ DataSegment.py
@@ -1,22 +1,71 @@
+def remove_prefix(node, prefix):
+    if node.data.startswith(prefix):
+        if len(node.data) > len(prefix):
+            return node.data[len(prefix):]
+        print(f"Node: {node.data} has no data after prefix: {prefix}")
+        return ""
+    print(f"Unexpected Prefix on Node: {node.data}, expected prefix: {prefix}")
+    return ""
+
+def verify_node(node, expected_data):
+    if node.data != expected_data:
+        print(f"Expected node: {expected_data}, found node: {node.data}")
+    return node
+
+
+class Entry():
+    def __init__(self, name, pos, value=None):
+        self.name = name
+        self.pos = pos
+        self.value = value
+
 class DataSegment():
     def __init__(self, symbol_table = None):
         self.map = {}
         self.size = 0
+        self.counter = 0
         if symbol_table:
             self.map_from_sym_table(symbol_table)
+ 
+    def visit_pass(self, node):
+        IMM_MAX = 2047  # TODO: determine actual immediate bounds
+        #########################
+        # Declist Node
+        #########################
+        if node.data == "DECLLIST":
+            d_typ = remove_prefix(node.children[0], "type:")
+            for child in node.children[1:]:
+                dec_node = verify_node(child, "DECLID")
+                if dec_node.children[0].data == "=":
+                    eq_node = verify_node(dec_node.children[0], "=")
+                    d_id = remove_prefix(eq_node.children[0], "id:")
+                    self.visit_pass(eq_node.children[1]) 
+                else:
+                    d_id = remove_prefix(dec_node.children[0], "id:")
+                self.map[d_id] = Entry(d_id, self.size)
+                self.size += 1  # All data is size 1w
+            return
+        ###########################
+        # Int/Float Val
+        ###########################
+        if "val" in node.data:
+            data = 0
+            if node.data.startswith("intval:"):
+                data = int(remove_prefix(node, "intval:"))
+                if data > IMM_MAX:
+                    self.map[f"!{self.counter}"] = Entry(f"!{self.counter}", self.size, data)
+                    self.size += 1
+                    self.counter += 1
+            elif node.data.startswith("floatval:"):
+                data = float(remove_prefix(node, "floatval:"))
+                self.map[f"!{self.counter}"] = Entry(f"!{self.counter}", self.size, data)
+                self.size += 1
+                self.counter += 1
 
-    def map_from_sym_table(self, symbol_table):
-        if symbol_table.tableStack[0]: 
-            global_scope = symbol_table.tableStack[0]
-            for var in global_scope:
-                (_, attr) = global_scope.table[var]
-                # Calc size/pos
-                var_size = 4
-                var_pos = self.size
-                # Grow Segment by size of var
-                self.size += var_size
-                # Add to map
-                self.map[var] = (pos, size) 
+            
+            return
+        for child in node.children:
+            self.visit_pass(child)
 
     def print_seg(self):
         for var in self.map:
--- czar.py
+++ czar.py
@@ -3,7 +3,7 @@ import sys
 import ParseTree
 from ReadAST import ReadAst
 from SymbolTable import SymbolTable
-from DataSegment import DataSegment
+from DataSegment import DataSegment, Entry
 
 num_GP_Reg = 0
 num_FP_Reg = 0
@@ -25,37 +25,42 @@ def main():
 
 		# Reading in the AST file into a ParseTree - Konch
 		root_AST: ParseTree = ReadAst(def_AST)
-
+		print(root_AST)
 		# Create Symbol table object, pass in the AST - Chris
-		sym_Table = SymbolTable()
-		sym_Table.visit_pass(root_AST)
-
+		
 		# Take from the symbol table and make dataSegments for it,
 		# make a map of symbols and literals to the dataspace stored - Chris
-		data_Seg = DataSegment(sym_Table)
+		data_Seg = DataSegment()
+		data_Seg.visit_pass(root_AST)
 
 		# Develop register needs - Konch
 		root_AST.registerNeeds()
 
 		# Using the register needs, develop - Konch
-		list_of_instructions_essential = treeCG(root_AST, num_GP_Reg, num_FP_Reg)
+		list_of_instructions = treeCG(root_AST, num_GP_Reg, num_FP_Reg)
 
 		# Do Jumps for ec
 
 		# Print to czr_OutFile
 		# data part
-		next_dataSegment: int = imageData(data_Seg, czr_OutFile)
+		imageData(data_Seg, czr_OutFile)
+
 		# instruction part
-		imageInit(list_of_instructions_essential, next_dataSegment, czr_OutFile)
+		imageInit(list_of_instructions, data_Seg, czr_OutFile)
 
 
 def imageData(data_segment, output):
-    for var in self.map:
-        (pos, _) = self.map[var]
-        output.write(f"label @{pos} {var}")
-        if "val" in var:
-            output.write(f"data @{pos} *Unimplemented*")    # TODO: support initial values
+    for var in data_segment.map:
+        entry = data_segment.map[var]
+        output.write(f"label @{entry.pos}w {entry.name}\n")
+        if entry.value:
+            output.write(f"data @{entry.pos}w {entry.value}\n")    # TODO: support initial values
 
+def treeCG(r, n, x):
+    return []
+
+def imageInit(a, b, c):
+    pass
 
 if __name__ == "__main__":
 	main()
